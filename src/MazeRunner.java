9092411f43e8dc4d5c4ebe926ce9bd8b3d56143d
abdel-elsayed <abdel.elsayed18@gmail.com>
2020-11-22 19:48:27 -0500

    added the user charcter on maze

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,6 +1,9 @@
 //package application;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 //import javafx.animation.Animation;
@@ -15,63 +18,158 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import java.lang.Math;
 
 
 public class MazeRunner extends Application
 {
-    public static final int TILE_SIZE = 50;
-    int start = 450;
+    private  int [][] data =
+                    { {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+                    , {1,0,1,0,1,0,1,0,1,0,0,0,0,1}
+                    , {1,0,1,0,0,0,1,0,1,0,1,1,1,1}
+                    , {1,0,0,0,1,1,1,0,0,0,0,0,0,1}
+                    , {1,0,1,0,0,0,0,0,1,1,1,1,0,1}
+                    , {1,0,1,0,1,0,1,0,1,0,1,0,0,1}
+                    , {1,0,1,0,1,1,1,0,1,0,0,0,1,1}
+                    , {1,0,0,0,0,1,0,0,1,0,1,0,0,0}
+                    , {1,0,1,1,0,1,0,0,1,1,1,0,0,1}
+                    , {1,0,0,1,0,1,1,0,0,0,0,0,0,1}
+                    , {1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+            };
 
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
+    public static final double TILE_SIZE = 50;
+    int start = 450;
+    private static final double W = 600, H = 400;
+    boolean running, goNorth, goSouth, goEast, goWest;
+    double x = 50;
+    double y = 50;
+    double radius = 10;
+    Circle hero = new Circle(x, y, radius);
+
+
 
     @Override
     public void start(Stage primaryStage) {
 
-        maze1 m = new maze1(10,10);
+        maze1 m = new maze1(600,400);
+        moveHeroTo(100, 85);
 
-        //Create
-        Pane mapGrid = m.createBoard();
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(m.createBoard());
+        Group root = new Group();
+        root.getChildren().addAll(m.createBoard(), hero);
 
-        //create test circle.
-        Circle circle = new Circle();
-        circle.setRadius(50.0f);
-
-        //Creating a Stackpane
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().addAll(new Rectangle(700,550, Color.WHITE), borderPane, circle);
-
-        //Add Stackpane to scene..
-        Scene scene = new Scene(stackPane);
-
-
-        ///Scene scene = new Scene(mapGrid);
+        Scene scene = new Scene(root);
+        System.out.println((int)x);
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                if (event.getCode() == KeyCode.UP) {
+                switch (event.getCode()) {
+                    case UP:
+                        //double checkPosition1 = y - 10;
+                        System.out.println("looking up: " + data[(int)x/30][((int)y/30)+1]);
+                        if( data[(int)x/30][((int)y/30) + 1] == 0 ){
+                            y -=10;
+                            System.out.println(x);
+                            System.out.println(y);
+                            hero.relocate(x, y);
+                        }
+                        break;
+                    case DOWN:
+                        //double checkPosition2 = y + 10;
 
-                    //List<Node> children = stackPane.getChildren();
-
-                    //path  testing..
-                    start -= 50;
-                    Path path = new Path();
-                    path.getElements().add(new MoveTo(start, 50));
-                    PathTransition pathTransition = new PathTransition();
-                    pathTransition.setDuration(Duration.millis(4000));
-                    pathTransition.setPath(path);
-                    pathTransition.setNode(circle);
-                    pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
-                    pathTransition.play();
+                        System.out.println("looking down: " + data[(int)x/30][((int)y/30)-1]);
+                        if( data[(int)x/30][((int)y/30) - 1] == 0 ){
+                            y +=10;
+                            System.out.println(x);
+                            System.out.println(y);
+                            hero.relocate(x, y);
+                        }
+                        break;
+                    case LEFT:
+                        //double checkPosition3 = x  - 10;
+                        System.out.println("looking Left: " + data[((int)x/30) - 1][(int)y/30]);
+                        if( data[(int)x/30 - 1][(int)y/30] == 0) {
+                            x -= 10;
+                            System.out.println(x);
+                            System.out.println(y);
+                            hero.relocate(x, y);
+                        }
+                        break;
+                    case RIGHT:
+                       // double checkPosition4 = x  + 10;
+                        System.out.println("looking right: " + data[((int)x/30) + 1][(int)y/30]);
+                        if( data[(int)x/30 + 1][(int)y/30] == 0 ) {
+                            x += 10;
+                            System.out.println(x);
+                            System.out.println(y);
+                            hero.relocate(x, y);
+                        }
+                        break;
+                    //case SHIFT: running = true; break;
                 }
             }
         });
+        /*
+        scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:
+                        hero.relocate(100, 5);
+                        //goNorth = false; break;
+                    //case DOWN:  goSouth = false; break;
+                    //case LEFT:  goWest  = false; break;
+                    //case RIGHT: goEast  = false; break;
+                    //case SHIFT: running = false; break;
+                }
+            }
+        });
+         */
+
         primaryStage.setTitle("Maze Runner");
         primaryStage.setScene(scene);
         primaryStage.show();
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                int dx = 0, dy = 0;
+
+                if (goNorth) dy -= 1;
+                if (goSouth) dy += 1;
+                if (goEast)  dx += 1;
+                if (goWest)  dx -= 1;
+                if (running) { dx *= 3; dy *= 3; }
+
+                moveHeroBy(dx, dy);
+            }
+        };
+        timer.start();
+    }
+    private void moveHeroBy(int dx, int dy) {
+        if (dx == 0 && dy == 0) return;
+        /*
+        final double cx = hero.getBoundsInLocal().getWidth() ;
+        final double cy = hero.getBoundsInLocal().getHeight();
+
+        double x = cx + hero.getLayoutX() + dx;
+        double y = cy + hero.getLayoutY() + dy;
+        */
+        moveHeroTo(1, 1);
+    }
+
+    private void moveHeroTo(double x, double y) {
+        final double cx = hero.getBoundsInLocal().getWidth();
+        final double cy = hero.getBoundsInLocal().getHeight();
+        System.out.println(hero.getBoundsInLocal().getWidth());
+        if (x - cx >= 0 &&
+                x + cx <= W &&
+                y - cy >= 0 &&
+                y + cy <= H) {
+            hero.relocate(x - cx, y - cy);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
