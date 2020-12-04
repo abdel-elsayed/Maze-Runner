04c84dd1cae86bc8d05c4c19075bdce55a758109
abdel-elsayed <abdel.elsayed18@gmail.com>
2020-12-04 18:29:25 -0500

    added timer functionality

--- src/MazeRunner.java
+++ src/MazeRunner.java
@@ -1,4 +1,7 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
@@ -10,22 +13,28 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.util.Duration;
 
 
 // The game runner
 public class MazeRunner extends Application
 {
     Scene scene1, scene2, scene3;
-    VBox vbox1, vbox2;
-    HBox nameBox, buttons;
-    Label title, label1;
+    VBox vbox1;
+    HBox nameBox, buttons, timer;
+    Label title, label1, timerText;
     TextField textField;
     Button button1, button2;
     Image bckGround, image1;
     Group root1, root2;
+    private static final Integer STARTTIME = 30;
+    Timeline timeline;
+    Label timerLabel = new Label();
+    Integer timeSeconds = STARTTIME;
 
     @Override
     public void start(Stage primaryStage) {
@@ -52,8 +61,34 @@ public class MazeRunner extends Application
                 + "-fx-padding: 5px 28px;\n");
 
         // events of the buttons
-        button1.setOnAction(e ->{ primaryStage.setScene(scene2);
-                    m.getPlayer().setName(textField.getText()); } );
+        button1.setOnAction(e ->{
+            if (timeline != null) {
+                timeline.stop();
+            }
+            timeSeconds = STARTTIME;
+
+            // update timerLabel
+            timerLabel.setText( timeSeconds.toString());
+            timeline = new Timeline();
+            timeline.setCycleCount(Timeline.INDEFINITE);
+            timeline.getKeyFrames().add(
+                    new KeyFrame(Duration.seconds(1),
+                            new EventHandler() {
+                                @Override// KeyFrame event handler
+                                public void handle(Event event) {
+                                    timeSeconds--;
+                                    // update timerLabel
+                                    timerLabel.setText(
+                                            timeSeconds.toString());
+                                    if (timeSeconds <= 0) {
+                                        timeline.stop();
+                                    }
+                                }
+                            }));
+            timeline.playFromStart();
+
+            primaryStage.setScene(scene2);
+            m.getPlayer().setName(textField.getText());});
         button2.setOnAction(e ->{
             System.exit(0);});
 
@@ -70,6 +105,20 @@ public class MazeRunner extends Application
         buttons.getChildren().add(button2);
         buttons.setSpacing(10);
         buttons.setAlignment(Pos.CENTER);
+
+        // timer
+        timerText = new Label("Time remaining: ");
+        timerText.setTextFill(Color.RED);
+        timer = new HBox(20);
+        timer.setAlignment(Pos.CENTER);
+        timerLabel.setText(timeSeconds.toString());
+        timerLabel.setTextFill(Color.RED);
+        timer.getChildren().addAll(timerText , timerLabel);
+        timer.setPrefWidth(200);
+        //timer.setStyle();
+        timer.setStyle("-fx-background-color: black;");
+
+
 
         // the background image in scene1
         bckGround = new Image("/background1.png");
@@ -120,7 +169,7 @@ public class MazeRunner extends Application
         status.setLayoutX(250);
 
         // adding the maze and the user to the root group // now new monsters spawn in aswell
-        root2.getChildren().addAll(m.getGrid(), hero, monster, monster2, monster3, monster4, monster5);
+        root2.getChildren().addAll(m.getGrid(), hero, monster, monster2, monster3, monster4, monster5, timer);
 
         //starts the movements of the monsters
         monster.move(1, m);
@@ -172,7 +221,6 @@ public class MazeRunner extends Application
                         }
                         if(m.getComputer().getIsWin() == true || m.getComputer2().getIsWin() == true || m.getComputer3().getIsWin() == true || m.getComputer4().getIsWin() == true) {
                             status.setText(" Hard Luck!!! " + hero.getName() + "\n YOU LOST!!!");
-
                             primaryStage.setScene(scene3);
                         }
                         //System.out.println(m.getComputer().getIsWin());
@@ -189,7 +237,14 @@ public class MazeRunner extends Application
                             status.setText(" Hard Luck!!! " + hero.getName() + "\n YOU LOST!!!");
                             primaryStage.setScene(scene3);
                         }
-                        //System.out.println(m.getComputer().getIsWin());
+                        if(timeSeconds == 0){
+                            m.getComputer().setIsWin(true);
+                            m.getComputer2().setIsWin(true);
+                            m.getComputer3().setIsWin(true);
+                            m.getComputer4().setIsWin(true);
+                            status.setText(" Hard Luck!!! " + hero.getName() + "\n YOU LOST!!!");
+                            primaryStage.setScene(scene3);
+                        }
                         break;
 
                 }
